5468dd13f3fa0277bf3639e15274c39f04dab80e
gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2020-04-25 23:57:36 -0400

    Changed links to be pulled in dynamically.  CSS and typo fixes.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate tera;
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
 use rocket_gatewaynode_com::*;
-use rocket_gatewaynode_com::models::{Post};
+use rocket_gatewaynode_com::models::{Post, Link};
 
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde_derive;
@@ -24,10 +24,23 @@ fn main() {
 #[get("/")]
 fn index() -> Template {
     #[derive(Serialize, Deserialize, Debug)]
-    struct PostList { posts: Vec<Post>, }
+    struct PostList {
+        posts: Vec<Post>,
+        regular_links: Vec<Link>,
+        thingy_links: Vec<Link>,
+        tut_links: Vec<Link>,
+    }
 
     let all_posts_raw = read_all_posts();
-    let all_posts = PostList{ posts: all_posts_raw };
+    let regular_links_raw = read_links_by_filter_limit(String::from("regular"), 10);
+    let thingy_links_raw = read_links_by_filter_limit(String::from("thingy"), 10);
+    let tut_links_raw = read_links_by_filter_limit(String::from("tut"), 10);
+    let all_posts = PostList{
+        posts: all_posts_raw,
+        regular_links: regular_links_raw,
+        thingy_links: thingy_links_raw,
+        tut_links: tut_links_raw,
+    };
 
     Template::render("front", &all_posts)
 }
