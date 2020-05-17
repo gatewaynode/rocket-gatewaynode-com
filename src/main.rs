a1e915593da08efe9bbc9c8df75a07a31f8cb224
gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2020-05-17 10:16:08 -0400

    Metafile improvements also added tabulator.js for indexing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@
 extern crate rocket_contrib;
 extern crate diesel;
 extern crate tera;
+// extern crate rocket_cors;
 
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
 use rocket::response::Redirect;
+use rocket::http::Method;
+use rocket::{get, routes};
 use rocket_gatewaynode_com::*;
 use rocket_gatewaynode_com::models::{Post, Link};
+// use rocket_cors;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_cors;
 #[macro_use] extern crate serde_derive;
 
 /// A simple implementation of Rocket for a personal blog.  Database content managed by https://github.com/gatewaynode/nautilus
@@ -35,8 +41,21 @@ struct LinkContentList {
 }
 
 fn main() {
+    let allowed_origins = AllowedOrigins::some_exact(&["https://commento.io", "http://localhost:8000","http://127.0.0.1:8000"]);
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS");
+
     rocket::ignite()
         .attach(Template::fairing())
+        .attach(cors)
         .mount("/", routes![
             index,
             favicon,
@@ -55,7 +74,7 @@ fn main() {
 
 #[get("/")]
 fn index() -> Template {
-    let all_posts_raw = read_all_posts();
+    let all_posts_raw = read_some_posts(10);
     let regular_links_raw = read_links_by_filter_limit(String::from("regular"), 10);
     let thingy_links_raw = read_links_by_filter_limit(String::from("thingy"), 10);
     let tut_links_raw = read_links_by_filter_limit(String::from("tut"), 10);
